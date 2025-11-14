256b3ec2fc36558a10d44930a484da82c4d93dee
Bharti <rathodbharti682@gmail.com>
2025-11-14 16:36:40 +0530

    Add Render domain to ALLOWED_HOSTS

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-o5+gsiqd(gwnyk31f#b90)b1$%ce@@(wj-cptckgc6x@qhtcd1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["personal-budget-tracker-backend-nd4p.onrender.com", "localhost", "127.0.0.1"]
+
 
 
 # Application definition
