d1552102282942c7234874980cfc5e37a098743f
Bharti <rathodbharti682@gmail.com>
2025-11-14 21:46:19 +0530

    Add temporary create-test-user endpoint

--- backend/urls.py
+++ backend/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include  # <-- include must be imported
+from finance.views import create_test_user 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
